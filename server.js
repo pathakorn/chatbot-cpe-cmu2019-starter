df955dd0a1d1d4b8b53b9e7efac71e618786285e
pathakorn <Pathakorn04@gmail.com>
2019-02-02 13:51:52 +0700

    webhook4

--- server.js
+++ server.js
@@ -16,18 +16,17 @@ app.get('/', function (req, res) {
 })
 
 app.post('/webhook', middleware(config), (req, res) => {
- // res.send('webhook success')
+  res.send('Hello World!!')
+const event = req.body.events[0];
+  if (event.type === 'message') {
+    const message = event.message;
+    client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: message.text
+    })
+   
+  }
 
-  const event = req.body.events[0];
-
-if (event.type === 'message') {
-  const message = event.message;
-  console.log(message)
-  client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: message.text
-  })
-}
 })
 
 app.set('port', (process.env.PORT || 4000))
