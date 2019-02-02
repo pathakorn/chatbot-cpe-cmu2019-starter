5035b35d1c194894189aa447d89a9703c55da884
pathakorn <Pathakorn04@gmail.com>
2019-02-02 14:01:34 +0700

    test log

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.post('/webhook', middleware(config), (req, res) => {
 const event = req.body.events[0];
   if (event.type === 'message') {
     const message = event.message;
-    console.log(message)
-    client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: message.text
-    })
+    console.log(message.text);
+    // client.replyMessage(event.replyToken, {
+    //   type: 'text',
+    //   text: message.text
+    // })
    
   }
 
