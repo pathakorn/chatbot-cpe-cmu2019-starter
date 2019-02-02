69df223043946cd5b0663326cae048514eebdf2a
pathakorn <Pathakorn04@gmail.com>
2019-02-02 15:18:33 +0700

    carousel

--- server.js
+++ server.js
@@ -34,42 +34,94 @@ const event = req.body.events[0];
 
 // })
 
-client.replyMessage(event.replyToken, 
-  {
-    "type": "template",
-    "altText": "This is a buttons template",
-    "template": {
-        "type": "buttons",
-        "thumbnailImageUrl": "https://lh3.googleusercontent.com/FyZA5SbKPJA7Y3XCeb9-uGwow8pugxj77Z1xvs8vFS6EI3FABZDCDtA9ScqzHKjhU8av_Ck95ET-P_rPJCbC2v_OswCN8A=s688",
-        "imageAspectRatio": "rectangle",
-        "imageSize": "cover",
-        "imageBackgroundColor": "#FFFFFF",
-        "title": "Pathakorn Rukchua",
-        "text": "Please select",
-        "defaultAction": {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://google.com/"
-        },
-        "actions": [
-            {
-              "type": "uri",
-              "label": "Facebook",
-              "uri": "https://www.facebook.com/pathakorn.rukchua?ref=bookmarks"
-            },
-            {
-              "type": "uri",
-              "label": "CPE Website",
-              "uri": "http://cpe.eng.cmu.ac.th/2013/"
-            },
-            // {
-            //   "type": "uri",
-            //   "label": "View detail",
-            //   "uri": "http://google.com"
-            // }
-        ]
-    }
-  })
+// client.replyMessage(event.replyToken, 
+//   {
+//     "type": "template",
+//     "altText": "This is a buttons template",
+//     "template": {
+//         "type": "buttons",
+//         "thumbnailImageUrl": "https://lh3.googleusercontent.com/FyZA5SbKPJA7Y3XCeb9-uGwow8pugxj77Z1xvs8vFS6EI3FABZDCDtA9ScqzHKjhU8av_Ck95ET-P_rPJCbC2v_OswCN8A=s688",
+//         "imageAspectRatio": "rectangle",
+//         "imageSize": "cover",
+//         "imageBackgroundColor": "#FFFFFF",
+//         "title": "Pathakorn Rukchua",
+//         "text": "Please select",
+//         "defaultAction": {
+//             "type": "uri",
+//             "label": "View detail",
+//             "uri": "http://google.com/"
+//         },
+//         "actions": [
+//             {
+//               "type": "uri",
+//               "label": "Facebook",
+//               "uri": "https://www.facebook.com/pathakorn.rukchua?ref=bookmarks"
+//             },
+//             {
+//               "type": "uri",
+//               "label": "CPE Website",
+//               "uri": "http://cpe.eng.cmu.ac.th/2013/"
+//             },
+//             // {
+//             //   "type": "uri",
+//             //   "label": "View detail",
+//             //   "uri": "http://google.com"
+//             // }
+//         ]
+//     }
+//   })
+// }
+// res.send("test")
+// })
+
+client.replyMessage(event.replyToken, {
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {  
+                    "type":"cameraRoll",
+                    "label":"Camera roll"
+                },
+                {  
+                  "type":"location",
+                  "label":"Location"
+               }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://c.76.my/Malaysia/line-brown-bear-cute-pencil-case-ubiyo-1802-02-Ubiyo@6.jpg",
+            "imageBackgroundColor": "#000000",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+              {
+                "type":"datetimepicker",
+                "label":"Select date",
+                "data":"storeId=12345",
+                "mode":"datetime",
+                "initial":"2017-12-25t00:00",
+                "max":"2018-01-24t23:59",
+                "min":"2017-12-25t00:00"
+              },
+              {  
+                "type":"camera",
+                "label":"Camera"
+             }
+          ]
+          }
+      ],
+      "imageAspectRatio": "rectangle",
+      "imageSize": "cover"
+  }
+})
 }
 res.send("test")
 })
